253b740b2b5402509de6f58e807e46a13349d13f
Felipe Nunes <fnap@cin.ufpe.br>
2025-12-18 16:09:09 -0300

    wip

--- sdk/serialtransport/reval-serial-transport/reval/serial/transport/__init__.py
+++ sdk/serialtransport/reval-serial-transport/reval/serial/transport/__init__.py
@@ -1,5 +1,6 @@
-from ._eventbus import EventBus, MessageType
+from ._eventbus import EventBus
 from ._transceiver import SerialTransceiver
+from ._message_type import MessageType
 
 __all__ = [
     "EventBus",
